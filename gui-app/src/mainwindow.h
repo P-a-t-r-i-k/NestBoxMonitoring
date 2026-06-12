35f86f8cc2f42d51ed7869d4966cbbb3aaa8983e
P-a-t-r-i-k <patrik.lasso.ml@gmail.com>
2026-06-12 19:58:31 +0200

    feat(gui-app): implement authentication

---
+++ gui-app/src/mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QCryptographicHash>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    enum class ConnectionState
+    {
+        Idle,
+        WaitingForNonce,
+        WaitingForAuthResult,
+        Authenticated
+    };
+
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onConnectButtonClicked();
+    void onSocketReadyRead();
+    void onSocketConnected();
+
+private:
+    void sendResponseFromNonce(QByteArray nonce);
+    void handleIncomingData(QByteArray data);
+    void handleSuccessfullAuthentication();
+    void handleFailedAuthentication();
+    void handleDisconnection();
+
+private:
+    Ui::MainWindow* m_ui;
+    QTcpSocket* m_socket;
+    ConnectionState m_currentConnectionState;
+};
+#endif // MAINWINDOW_H
